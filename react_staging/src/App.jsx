661c49377f9bdabf1471ebf210115db4a4ef59c7
tss <517739813@qq.com>
2023-02-15 17:37:13 +0800

    2023.2.15写了github案列和用户接口请求，以及重温代理

--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import './App.css';
+import Search from './components/Search';
+import List from './components/List';
+
 export default class App extends Component {
 
 	render() {
 		return (
 			<div>
-				App....
-
+				<div className="container">
+					<Search />
+					<List />
+				</div>
 			</div >
 		)
 	}
---
+++ react_staging/src/components/Search/index.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+export default class Search extends Component {
+
+    search = () => {
+        const { keyWordNode: { value } } = this //连续解构赋值写法
+        console.log(value);
+        //站在3000端口的话向localhost:3000发送都不用写这个，可以忽略掉
+        axios.get(`http://localhost:3000/api1/search/users?q=${value}`).then((response) => {
+                console.log(response.data)
+         }, 
+         error => { console.log(error); })
+    }
+
+    render() {
+        return (
+            <section className="jumbotron">
+                <h3 className="jumbotron-heading">Search Github Users</h3>
+                <div>
+                    <input ref={e => this.keyWordNode = e} type="text" placeholder="输入关键词点击搜索" />&nbsp;
+                    <button onClick={this.search}>Search</button>
+                </div>
+            </section>
+        )
+    }
+}
