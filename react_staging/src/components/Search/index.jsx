055a9145651e722cde2b93d45ac4ac1943396d19
tss <517739813@qq.com>
2023-02-16 15:53:40 +0800

    添加记事本关于github案例搜索信息内容

--- react_staging/src/components/Search/index.jsx
+++ react_staging/src/components/Search/index.jsx
@@ -3,16 +3,27 @@ import axios from 'axios'
 export default class Search extends Component {
 
     search = () => {
-        const { keyWordNode: { value } } = this //连续解构赋值写法
-        console.log(value);
+        const { keyWordNode: { value: values } } = this //连续解构赋值重命名写法
+        // console.log(value);
+        // 发送请求钱通知app更新状态
+        //发送请求前通知App更新状态
+        this.props.updateAppState({ isFirst: false, isLoading: true })
+
+
+
         //https://api.github.com/search/users?q=atguigu github原接口
         //axios.get(`https://api.github.com/search/users?q=${value}`)
         //站在3000端口的话向localhost:3000发送都不用写这个，可以忽略掉
-        axios.get(`api1/search/users?q=${value}`).then(
+        axios.get(`api1/search/users?q=${values}`).then(
             response => {
-                this.props.saveUsers(response.data.items)
+                this.props.updateAppState({ isLoading: false, users: response.data.items })
             },
-            error => { console.log(error); })
+            error => {
+                // 请求失败后更新app状态
+                this.props.updateAppState({
+                    isLoading: false, err: error.message
+                })
+            })
     }
 
     render() {
