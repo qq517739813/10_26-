e8858819ae730c83b627aee9833d340c4a3da87f
tss001 <517739813@qq.com>
2023-03-03 18:03:29 +0800

    描述

--- react_staging/src/index.js
+++ react_staging/src/index.js
@@ -3,12 +3,12 @@ import React from 'react'
 //引入ReactDOM
 // import ReactDOM from 'react-dom'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
 //引入App
 import App from './App'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(
-    <BrowserRouter><App /></BrowserRouter>
+    <App />
 )
