2770126bfce448fb7cc6b3068760ad5058733396
tss001 <517739813@qq.com>
2023-03-07 17:59:43 +0800

    antd

--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
+import { Button } from 'antd';
+import {
+	SettingOutlined,
+	WechatOutlined
+} from '@ant-design/icons';
 
 export default class App extends Component {
-  render() {
-	return (
-	  <div>
-		<div>App</div>
-		<button>点我</button>
-	  </div>
-	)
-  }
+	render() {
+		return (
+			<div>
+				<h2>123</h2>
+				<Button type="primary">Primary</Button>
+				<Button>Default</Button>
+				<Button type="dashed">Dashed</Button>
+				<Button type="danger">Danger</Button>
+				<Button type="link">Link</Button>
+				{/* <Icon type="retweet" /> */}
+				<SettingOutlined />
+				<WechatOutlined />
+
+			</div>
+		)
+	}
 }
