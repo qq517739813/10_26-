a852cc796769455795978c4dc74cdccef9f1398e
tss001 <517739813@qq.com>
2023-03-08 18:09:10 +0800

    andt

--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
-import { Button } from 'antd';
+import { Button ,DatePicker } from 'antd';
 import {
 	SettingOutlined,
-	WechatOutlined
+	WechatOutlined,
+	SearchOutlined,
+	
 } from '@ant-design/icons';
 
 export default class App extends Component {
+
+	onChange = (e,datestring) => { console.log(e,'分割',datestring); }
 	render() {
 		return (
 			<div>
 				<h2>123</h2>
-				<Button type="primary">Primary</Button>
+				<Button type="primary">Primary<SearchOutlined /></Button>
 				<Button>Default</Button>
 				<Button type="dashed">Dashed</Button>
 				<Button type="danger">Danger</Button>
@@ -18,6 +22,7 @@ export default class App extends Component {
 				{/* <Icon type="retweet" /> */}
 				<SettingOutlined />
 				<WechatOutlined />
+				<DatePicker onChange={this.onChange} />
 
 			</div>
 		)
