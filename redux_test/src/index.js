bc828e9ba55ee4a2352a5cb27283ef5472451d5b
tss001 <517739813@qq.com>
2023-03-09 14:07:45 +0800

    redux

---
+++ redux_test/src/index.js
@@ -0,0 +1,14 @@
+//引入react核心库
+import React from 'react'
+//引入ReactDOM
+// import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
+// import { BrowserRouter } from 'react-router-dom'
+//引入App
+import App from './App'
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(
+    <App />
+)
